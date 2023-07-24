600c326e559f3771ca618521a83baf5d35829424
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-24 15:32:36 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,75 @@
-# importing all the essential modules
-import pygame, sys, os
-from pygame.locals import *
+import pygame
+import sys
 
-# setting the colors using RGB values
-red = [255, 0, 0]
-
-# initializing pygame
+# Initialize Pygame
 pygame.init()
 
-# setting up pygame window
-window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption("Hello this is pygame window")
+# Game settings
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+FPS = 60
 
-# setting up a drawing surface
-screen = pygame.display.get_surface()
-screen.fill(red)
-pygame.display.set_caption("Snake")
-pygame.display.flip()
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-while True:
-    pass
+# Create the game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pygame Learning")
+
+# Game variables
+clock = pygame.time.Clock()
+running = True
+
+# Player variables
+player_width, player_height = 50, 50
+player_x = SCREEN_WIDTH // 2 - player_width // 2
+player_y = SCREEN_HEIGHT - player_height
+player_speed = 5
+
+# Main game loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Handle player movements
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+    if keys[pygame.K_UP]:
+        player_y -= player_speed
+    if keys[pygame.K_DOWN]:
+        player_y += player_speed
+
+    # Prevent the player from moving outside the window
+    player_x = max(0, min(player_x, SCREEN_WIDTH - player_width))
+    player_y = max(0, min(player_y, SCREEN_HEIGHT - player_height))
+
+    # Draw everything on the screen
+    screen.fill(BLACK)
+
+    # Draw player
+    pygame.draw.rect(screen, RED, (player_x, player_y, player_width, player_height))
+
+    # Draw shapes (e.g., circle, line, etc.)
+    pygame.draw.circle(screen, GREEN, (100, 100), 30)
+    pygame.draw.line(screen, BLUE, (200, 200), (300, 300), 3)
+
+    # Display text
+    font = pygame.font.SysFont(None, 30)
+    text = font.render("Hello, Pygame!", True, WHITE)
+    screen.blit(text, (10, 10))
+
+    pygame.display.update()
+
+    # Control the frame rate
+    clock.tick(FPS)
+
+# Quit Pygame
+pygame.quit()
+sys.exit()
